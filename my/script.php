3291f1fea3163cc4777b79e1ed71127ed478276a
Anton Lukin <anton@lukin.me>
2013-12-23 08:04:18 +0400

    update my / add item

--- my/script.php
+++ my/script.php
@@ -28,42 +28,27 @@ function check_item($start, $cron){
 	return $diff % $cron === 0;
 }
  
-function query_close(){
+function query_close($link){
 
 	if(!isset($_POST['item']) || empty($_POST['item']))
 		halt_app(array('message' => 'Не указан идентификатор задачи'));
-
- 	if(!$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
-		return false;
-
-	mysqli_set_charset($link, "utf8");
 
 	$item = mysqli_real_escape_string($link, (int)$_POST['item']);
 	if(!mysqli_query($link, "INSERT INTO log (item_id) VALUES ('{$item}')"))
 		halt_app(array('message' => 'Не удалось обновить задачу'));     		
 }
 
-function query_open(){
+function query_open($link){
 
 	if(!isset($_POST['item']) || empty($_POST['item']))
 		halt_app(array('message' => 'Не указан идентификатор задачи'));
-
- 	if(!$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
-		return false;
-
-	mysqli_set_charset($link, "utf8");
 
 	$item = mysqli_real_escape_string($link, (int)$_POST['item']);
 	if(!mysqli_query($link, "DELETE FROM log WHERE item_id = '{$item}' AND DATE(time) = CURRENT_DATE"))
 		halt_app(array('message' => 'Не удалось обновить задачу'));     		  
 }
 
-function query_normal($result){
-
-  	if(!$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
-		return false; 
-
-	mysqli_set_charset($link, "utf8");  
+function query_normal($link, $result){
 
 	if(!$query = mysqli_query($link, "SELECT DISTINCT item_id as id FROM log WHERE DATE(time) = CURRENT_DATE"))
 		halt_app(array('message' => 'Не удалось получить список задач')); 
@@ -89,12 +74,7 @@ function query_normal($result){
 	halt_app(array('message' => $answer, 'success' => TRUE));  
 }
 
-function query_manager($result){
-
-  	if(!$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
-		return false; 
-
-	mysqli_set_charset($link, "utf8");  
+function query_manager($link, $result){
 
 	if(!$query = mysqli_query($link, "SELECT id, name, cron, DATE(start) as start, title FROM items"))
 		halt_app(array('message' => 'Не удалось получить список задач'));
@@ -107,16 +87,55 @@ function query_manager($result){
 	halt_app(array('message' => $answer, 'success' => TRUE));  
 } 
 
+function query_add($link, $set){
+	$valid = array('start', 'name', 'cron', 'title');
+
+	foreach($_POST as $i => $v)	{
+		if(empty($v) || !in_array($i, $valid))
+			continue;
+
+		$set[] = "$i = '" . mysqli_real_escape_string($link, $v) . "'";
+	}
+	$set = implode(", ", $set);
+
+	if(!mysqli_query($link, "INSERT INTO items SET {$set}"))
+		halt_app(array('message' => 'Не удалось добавить задачу'));   
+
+	halt_app(array('message' => 'Задача успешно добавлена', 'success' => TRUE));
+}
+
+function query_delete($link){
+	
+	if(!isset($_POST['item']) || empty($_POST['item']))
+		halt_app(array('message' => 'Не указан идентификатор задачи'));
+
+	$item = mysqli_real_escape_string($link, (int)$_POST['item']);
+	if(!mysqli_query($link, "DELETE FROM items WHERE id = '{$item}'"))
+		halt_app(array('message' => 'Не удалось удалить задачу'));        
+}
+
 function request_uri($url){
-	$locations = array("normal" => "query_normal", "close" => "query_close", "open" => "query_open", "manager" => "query_manager");
+	$locations = array(
+		"normal" => "query_normal", 
+		"close" => "query_close", 
+		"add" => "query_add", 
+		"delete" => "query_delete",  
+		"open" => "query_open", 
+		"manager" => "query_manager"
+	);
 
 	preg_match("~^[a-z0-9]+~", $url, $uri);
 	$uri = array_shift($uri);                                    
 
 	if(!array_key_exists($uri, $locations) || !function_exists($execution = $locations[$uri]))
 		halt_app(array('message' => 'Действие не определено'));
+   
+	if(!$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
+		halt_app(array('message' => 'Невозможно соединиться с базой данных'));           
 
-	$execution();
+	mysqli_set_charset($link, "utf8");    
+
+	$execution(&$link);
 }        
 
 {
--- public/assets/scripts/my.js
+++ public/assets/scripts/my.js
@@ -1,4 +1,7 @@
 var $ = jQuery.noConflict();
+ 
+var reminder = $("#reminder"),
+	preloader = $("#preloader");           
 
 function error(text) {
 	var popup = jQuery('#popup'),
@@ -18,27 +21,21 @@ function error(text) {
 	});
 }
 
-function status(action, id){
- 	$.ajax({
-		type: 'POST', url: '/' + action, data:{item:id},
-		error: function(){
-			return error('Не удалось связаться с сервером');
-		},
-		success: function(data){
+function query(action, vals){
+ 	$.post('/' + action, vals,
+	function(data){
+   		if(data.error)
+   			return error(data.error); 
 
-			if(data.error)
-				return error(data.error); 
-
-			return true;
-		}
-	});                  
+		if(action === 'add')
+			reminder.find("li.new input").val('');
+	
+   		return true;
+   	}, 'json');
 }
 
-function init(){ 
-	var hash = document.location.hash.replace('#', ''),
-		manage = (hash === 'manager');
-		page = manage ? 'manager' : 'normal';
-
+function init(manage){ 
+	var page = (typeof manage === 'undefined' || !manage) ? 'normal' : 'manager';
 
   	$.ajax({
 		type: 'POST', url: '/' + page,
@@ -56,8 +53,11 @@ function init(){
 			var response = $.parseJSON(data.success);
 			$.each(response, function(i, v){
 				var item = $(document.createElement('li'))
-				$('<h2>' + v.name + '</h2>').appendTo(item);
- 				$('<h3>' + v.title + '</h3>').appendTo(item); 
+                var content =  '<h2>' + v.name + '</h2>' + '<h3>' + v.title + '</h3>';
+					content += '<b>×</b>' + '<div>' + v.start + ' / ' + v.cron + '</div>';
+
+				$(content).appendTo(item);
+			   
 
   				item.data('id', v.id); 
 				item.data('start', v.start);
@@ -72,33 +72,35 @@ function init(){
 			if($("#done > li").length > 0)
 				$("#done-title").show();
 
-			$("#reminder").addClass(page);
+			if(manage)
+				$("#todo .new").show();
+			else
+				$("#todo .new").hide(); 
+
+			reminder.removeClass().addClass(page);
 		}
 	}); 
 }
 
-$(window).on('hashchange', function(){
+function board(act){
 
-	$("#reminder").fadeOut(function(){
-		$("#preloader").fadeIn(function(){
+   	reminder.fadeOut(function(){
+		reminder.find('ul li').not('.new').remove();
+	 	preloader.fadeIn(function(){
 
-			window.location.reload();
+			$.when(init(act)).then(function(){
+				preloader.fadeOut(function(){   
+					reminder.fadeIn();
+				});
+			}); 
 
 		});
-	});   
+	}); 
 
-});
-
-
+}
+  
 $(window).load(function(){
-
-	$.when(init()).then(function(){
-
-		$("#preloader").fadeOut(function(){
-			$("#reminder").fadeIn();
-		});
-
-	});
+	board(false);
 });
 
 $(document).ready(function(){
@@ -110,10 +112,23 @@ $(document).ready(function(){
 			if($("#done > li").length < 1)
 				$("#done-title").show();
 
-			status('close', id);
+			query('close', {item:id});
 				
 			$(this).appendTo("#done").fadeIn();
 		});				
+	}); 
+
+	$("header").on('click', 'p#options img', function(){
+		var settings = reminder.prop('class') !== 'manager',
+			src = $(this).prop('src'),
+			icon = settings ? src.replace('cogs', 'refresh') :  src.replace('refresh', 'cogs');
+			
+		$(this).fadeOut(function(){
+			$(this).prop('src', icon).fadeIn(700);
+		});
+		board(settings);
+
+		return false;
 	});
 	
 	$(document.body).on('click', '#reminder.normal #done li', function(){
@@ -122,11 +137,58 @@ $(document).ready(function(){
 		$(this).fadeOut( function(){		
 			$(this).appendTo("#todo").fadeIn();
 			
- 			status('open', id); 
+ 			query('open', {item:id}); 
 
 			if($("#done > li").length < 1)
 				$("#done-title").hide();
 		});				
 	});
+
+	$(document.body).on('click', '#reminder.manager li b', function(){
+		var li = $(this).closest('li');
+
+		li.fadeOut(function(){
+			query('delete', {item:li.data('id')});
+		});
+		
+	});      
+
+	$(document.body).on('keypress', '#reminder.manager li input', function(e){
+		if(e.which !== 13)
+			return;
+
+		var li = $(this).closest('li'),
+			item = new Object(),
+			valid = true;
+
+		li.find('input').each(function(i, v){
+			$(v).removeClass('invalid');
+            
+			var v = $(v);
+				n = v.prop('class').replace('item-', '');
+
+			item[n] = v.val();
+
+			if(!v.prop('pattern'))
+				return;
+
+			if(v.val().match(v.prop('pattern')))
+				return;
+		
+			if(n == 'start' && v.val() == '')
+				return;
+				
+			valid = false;
+			v.addClass('invalid');
+		});
+
+		if(!valid)
+			return false;
+
+		$.when(query('add', item)).then(function(){  
+ 		   	board(true);
+		});
+		
+	});       
 	
 }); 
