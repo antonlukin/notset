ee6cc01cff0c8d4b8c2231731a8cb8067800ef01
Anton Lukin <anton@lukin.me>
2017-10-08 23:34:15 +0300

    test js ip script

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
-(function(){
+function foo(data) {
+ 	console.log(data);
+}
+
+(function() {
+
+var script = document.createElement('script');
+script.src = '//ipinfo.io/?callback=foo'
+//script.src = 'https://geoip.nekudo.com/api/?callback=foo';
+
+document.getElementsByTagName('head')[0].appendChild(script);
+
 })();
