63d429f8c1ca1f20183a865de6b08e69942d2941
Anton Lukin <anton@lukin.me>
2017-10-12 23:43:24 +0300

    update code view on ip template

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,31 @@
-function foo(data) {
- 	console.log(data);
+var ip = {
+	api: '//ipinfo.io/',
+
+	back: function(data) {
+		console.log(data);
+//		document.querySelector(".info__code").innerHTML = JSON.stringify(data);
+
+		var ul = document.createElement("ul")
+		ul.className = 'info__code';
+
+		for(var key in data) {
+			var li = document.createElement('li');
+			li.innerHTML = '<span>' + key + '</span>: <span> ' + data[key] + '</span>';
+
+			ul.appendChild(li);
+		}
+
+		document.querySelector(".info").appendChild(ul);
+	},
+
+	init: function() {
+		var script = document.createElement('script');
+
+		var i = document.querySelector(".info__head").innerHTML;
+
+		script.src = ip.api + i + '/?callback=ip.back';
+		document.getElementsByTagName('head')[0].appendChild(script);
+	}
 }
 
-(function() {
-
-var script = document.createElement('script');
-script.src = '//ipinfo.io/?callback=foo'
-script.src = 'https://geoip.nekudo.com/api/?callback=foo';
-
-document.getElementsByTagName('head')[0].appendChild(script);
-
-})();
+ip.init();
