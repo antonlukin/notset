9ad30da0495a7d07015af336e5131b8aceda88d2
Anton Lukin <anton@lukin.me>
2019-06-12 17:31:37 +0400

    replace npm with yarn

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 let gulp      = require('gulp');
 let sass      = require('gulp-sass');
 let concat    = require('gulp-concat');
-let minify    = require('gulp-minify-css');
+let minify    = require('gulp-clean-css');
 let uglify    = require('gulp-uglify-es').default;
 let plumber   = require('gulp-plumber');
 let flatten   = require('gulp-flatten');
@@ -14,31 +14,39 @@ let path = {
   assets: 'public/assets/'
 }
 
-gulp.task('scss', function() {
-  gulp.src([path.source + 'scss/app.scss'])
+gulp.task('scss', function(done) {
+  gulp.src(path.source + 'scss/app.scss')
     .pipe(plumber())
     .pipe(sass({errLogToConsole: true}))
-    .pipe(prefix({browsers: ['ie >= 10','ff >= 30', 'chrome >= 34', 'safari >= 7', 'ios >= 7']}))
+    .pipe(prefix(['ie >= 10','ff >= 30', 'chrome >= 34', 'safari >= 7', 'ios >= 7']))
     .pipe(concat('styles.min.css'))
     .pipe(minify({compatibility: 'ie8'}))
     .pipe(gulp.dest(path.assets))
+
+  done();
 })
 
-gulp.task('js', function() {
-  gulp.src([path.source + '/js/*.js'])
+gulp.task('js', function(done) {
+  gulp.src(path.source + '/js/*.js')
     .pipe(plumber())
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(path.assets))
+
+  done();
 })
 
-gulp.task('images', function() {
-  gulp.src([path.source + '/images/**/*'])
+gulp.task('images', function(done) {
+  gulp.src(path.source + '/images/**/*')
     .pipe(gulp.dest(path.assets + '/images/'));
+
+  done();
 })
 
-gulp.task('watch', function() {
-  gulp.watch(path.source + '/**/*', ['scss', 'js', 'images']);
+gulp.task('watch', function(done) {
+  gulp.watch(path.source + '/**/*', gulp.parallel('scss', 'js', 'images'));
+
+  done();
 })
 
-gulp.task('default', ['js', 'scss', 'images', 'watch']);
+gulp.task('default', gulp.series('js', 'scss', 'images', 'watch'));
